d19905c3d595266d205df791abe8defe5b79f3fc
Vasiliy Kryuchkov <vvvvk@yandex.ru>
2023-01-24 22:42:00 +0400

    config

---
+++ config.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+from environs import Env  # type: ignore
+
+
+@dataclass
+class TgBot:
+    token: str
+
+
+@dataclass
+class Config:
+    bot: TgBot
+
+
+def load_cfg(path: str | None) -> Config:
+    env = Env()
+    env.read_env(path)
+    config = Config(TgBot(env('BOT_TOKEN')))
+    return config
